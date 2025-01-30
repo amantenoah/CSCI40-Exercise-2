7b763be2a4907b62517ca7d0aea02f9b6f2f13f7
amantenoah <111347849+amantenoah@users.noreply.github.com>
2025-01-30 22:29:43 +0800

    added a test file and fixed the constructor after realizing python does not support multiple constructors

--- Fraction.py
+++ Fraction.py
@@ -1,19 +1,39 @@
 class Fraction(object):
 
-    def __init__(self, numerator, denominator):
-        self.numerator = numerator
-        self.denominator = denominator
-
-    def __init__(self, fraction):
-        numerator = fraction[0]
-        denominator = fraction[2]
-        self.fraction = numerator/denominator
-
-    def __init__(self, number):
-        numerator = number
-        denominator = 1
-        self.fraction = numerator/denominator
+    def __init__(self, numerator=None, denominator=None):
+        # Handle standard two value inputs
+        if denominator is not None:
+            if not (isinstance(numerator, int) and isinstance(denominator, int)):
+                raise TypeError("Both numerator and denominator must be integers.")
+            self.numerator = numerator
+            self.denominator = denominator
+            if self.denominator == 0:
+                raise ValueError("Denominator cannot be 0")
+            
+        # Handle single value inputs, i.e. rational number or strings
+        # Rationale number input    
+        elif isinstance(numerator, int):
+            self.numerator = numerator
+            self.denominator = 1
+            
+        elif isinstance(numerator, str):
+            fraction = numerator.strip() # Remove any unecessary white spaces
+            # Find the / that divides numerator and denominator, assign values after
+            divisionIndex = 0
+            for digit in numerator:
+                if digit == "/":
+                    divisionIndex = digit.index()
+                    
+            self.numerator = numerator[:divisionIndex]
+            self.denominator = numerator[divisionIndex:]
+            if self.denominator == 0:
+                raise ValueError("Denominator cannot be 0")
                 
+        # Handle remaining invalid inputs
+        else:
+            raise TypeError("Invalid input, refer to instructions for proper syntax")
+        
+        
     def gcd(a, b):
         if a == 0 or b == 0:
             return 0
---
+++ test.py
@@ -0,0 +1,9 @@
+'''
+Test file for testing specific features manually
+before comparing against full test cases
+'''
+
+from Fraction import Fraction
+
+f1 = Fraction(2, 4)
+print(f1.gcd(2,4))
